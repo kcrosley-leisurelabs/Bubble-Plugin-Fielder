c4390ecc97f32dec570e3674bcc9b86f28fa5d83
Keith Crosley <keithcrosley@icloud.com>
2022-11-16 21:01:21 -0800

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -3,16 +3,18 @@ function(instance, context) {
 
         // initialize here
         instance.data.initialized = null
-        instance.data.system_fields = ['Created By', 'Slug', 'Created Date', 'Modified Date', '_id']
-        instance.data.doubles = [/_date_range(?!.*_date_range)/, /_number_range(?!.*_number_range)/, /_geographic_address(?!.*_geographic_address)/]
+        instance.data.system_fields = ['Created By', 'Slug', 'Created Date', 'Modified Date', '_id', 'email', 'logged_in']
+    instance.data.doubles = [/_date_range(?!.*_date_range)/, /_number_range(?!.*_number_range)/, /_geographic_address(?!.*_geographic_address)/, /_option(?!.*_list_option).*/]
+        instance.data.underscores = /_+/g
 
         instance.data.funcUpdate = function update(instance, props) {
-            // instance.data.start = Date.now() // uncomment to check update speed (it's 0-1 ms)
+            if (instance.data.debug) instance.data.start = Date.now()
             // move properties to instance
             instance.data.funcGetProps(instance, props)
 
-            instance.data.fields = instance.data.things.map(thing => thing && thing.hasOwnProperty('get') && thing.hasOwnProperty('listProperties') && thing.listProperties())
-            
+            instance.data.fields = instance.data.things.map(thing => thing && thing.get && thing.listProperties && thing.listProperties())
+            instance.data.debug && console.log('Before processing Bubble fields are as follows: ', JSON.stringify(instance.data.fields))
+
             instance.data.funcProcessFields(instance, props)
             instance.data.funcPublishStates(instance)
 
@@ -20,7 +22,7 @@ function(instance, context) {
             instance.publishState('initialized', instance.data.initialized)
             instance.triggerEvent('initialized')
 
-            // console.log('init time: ', Date.now() - instance.data.start) // uncomment to check update speed (it's 0-1 ms)
+            instance.data.debug && console.log('Processing time: ', Date.now() - instance.data.start)
 
         }
 
@@ -34,35 +36,50 @@ function(instance, context) {
         instance.data.funcGetProps = function getProps(instance, props) {
             // move main properties into instance
             instance.data.things = Object.getOwnPropertyNames(props).filter(prop => prop.includes('thing')).map(prop => props[prop])
+            instance.data.debug = props.debug
         }
             
         instance.data.funcProcessFields = function processFields(instance, props) {
-            const regx = /_[^_]+$/
-            const regx2 = /_custom(?!.*_custom)/
+            const regx = /_[^_]+$/ // matches last _something
+            const regx2 = /_custom(?!.*_custom).*/
+            const regx3 = /_doublz(?!.*_doublz).*/
+            const regx4 = /_list(?!.*_list).*/
             instance.data.fields.forEach((fields, i) => {
                 if (!fields)
                     return
                 //no_system
-                var system_fields = fields.filter(field => instance.data.system_fields.includes(field))
+                var system_fields = []
+                system_fields[i] = fields.filter(field => instance.data.system_fields.includes(field))
                 instance.data.fields[i] = fields.filter(field => !instance.data.system_fields.includes(field))
                 //remove_type
                 if (props.remove_type) {
                     // remove "doubles"
                     instance.data.fields[i] = instance.data.fields[i].map(field => {
-                        instance.data.doubles.forEach(regex => field = field.replace(regex, '_double'))
+                        instance.data.doubles.forEach(regex => field = field.replace(regex, '_doublz'))
                         return field
                     })
-                    instance.data.fields[i] = instance.data.fields[i].map(field => field.replace(regx, '').replace(regx2, ''))
+                    // remove _custom and any remaining _list
+                    instance.data.fields[i] = instance.data.fields[i].map(field => {
+                        var newfield = field.replace(regx2, '_custom').replace(regx, '')
+                        if (newfield.endsWith('_list')) newfield = newfield.replace(regx4, '')
+                        return newfield
+                    })
                 }
                 //title_case
-                if (props.title_case)
-                    instance.data.fields[i] = instance.data.fields[i].map(field => field.replaceAll('_', ' ').split(' ').map(word => word.replace(word[0], word[0].toUpperCase())).join(' '))
+                if (props.title_case) {
+                    instance.data.fields[i] = instance.data.fields[i].map(field => field.replaceAll(instance.data.underscores, '_').replaceAll(instance.data.underscores, ' ').trim().split(' ').map(word => word.replace(word[0], word[0].toUpperCase())).join(' '))
+                }
+                instance.data.debug && console.log('After processing, custom fields have become: ', JSON.stringify(instance.data.fields))
                 //put system fields back, if supposed to
                 if (!props.no_system) {
-                    instance.data.fields[i] = instance.data.fields[i].concat(system_fields)
+                    instance.data.fields[i] = instance.data.fields[i].concat(system_fields[i])
                     if (props.title_case) {
                         var idIndex = instance.data.fields[i].indexOf('_id')
                         if (idIndex > -1) instance.data.fields[i][idIndex] = 'Unique ID'
+                        var emailIndex = instance.data.fields[i].indexOf('email')
+                        if (emailIndex > -1) instance.data.fields[i][emailIndex] = 'Email'
+                        var loggedIndex = instance.data.fields[i].indexOf('logged_in')
+                        if (loggedIndex > -1) instance.data.fields[i][loggedIndex] = 'Logged In'
                     }
                 }
             })
